dbfda80ed88234fa21ed972c746815a14347f8d8
Nate Holland <natesholland@gmail.com>
2019-01-12 21:25:15 -0600

    finished with state lesson

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -4,7 +4,9 @@ class Order extends React.Component {
 
   render() {
     return (
-      <div className="order">Order</div>
+      <div className="order">
+        <h2>Order</h2>
+      </div>
     )
   }
 
