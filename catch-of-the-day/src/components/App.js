122c610009601c9c4587e1d8cd9441ad529cff33
Nate Holland <natesholland@gmail.com>
2019-01-20 12:11:54 -0600

    finished another one

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes"
+import Fish from "./Fish"
 
 class App extends React.Component {
   state = {
@@ -22,14 +23,26 @@ class App extends React.Component {
     this.setState({fishes: sampleFishes});
   }
 
+  addToOrder = key => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to the order, or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish addToOrder={this.addToOrder} index={key} key={key} details={this.state.fishes[key]}/>)}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
 
       </div>
     )
---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { formatPrice } from "../helpers";
+
+class Fish extends React.Component {
+
+  render() {
+    const {image, name, status, price, desc} = this.props.details;
+    const isAvailable = status === 'available';
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">{name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button onClick={() => this.props.addToOrder(this.props.index)} disabled={!isAvailable}>{isAvailable ? 'Add To Cart' : 'Sold Out'}</button>
+      </li>
+    )
+  }
+
+}
+
+export default Fish;
